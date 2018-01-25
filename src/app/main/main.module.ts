084a172285827b4afa2352bed9e596f92d4d0c7e
eduardomamedio <eduardomamedio@gmail.com>
2018-01-24 22:40:05 -0200

    Aula03

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { MainComponent } from './main.component';
 import { RouterModule } from '@angular/router';
 import { MainRouting } from './main.routing';
-import {MatIconModule, MatListModule, MatSidenavModule, MatToolbarModule, MatCardModule, MatButtonModule, MatTableModule} from '@angular/material';
+import { MatIconModule, MatListModule, MatSidenavModule, MatToolbarModule, MatCardModule, MatButtonModule, MatTableModule, MatTooltipModule, MatFormFieldModule, MatOptionModule, MatSelectModule, MatInputModule} from '@angular/material';
 import { FormularioComponent } from './usuario/formulario/formulario.component';
 import { ConsultaComponent } from './usuario/consulta/consulta.component';
+import { FlexLayoutModule} from '@angular/flex-layout'
 
 @NgModule({
   imports: [
@@ -18,8 +19,14 @@ import { ConsultaComponent } from './usuario/consulta/consulta.component';
     MatToolbarModule,
     MatCardModule,
     MatButtonModule,
-    MatTableModule
+    MatTableModule,
+    MatTooltipModule,
+    MatFormFieldModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatInputModule,
+    FlexLayoutModule
   ],
-  declarations: [MainComponent, ConsultaComponent]
+  declarations: [MainComponent, ConsultaComponent, FormularioComponent]
 })
 export class MainModule { }
